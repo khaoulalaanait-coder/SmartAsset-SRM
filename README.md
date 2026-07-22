c5716574e00a40bfad66edfc9c39d780b3405db1
Khaoula Laanait <khaoula.laanait@gmail.com>
2026-07-22 12:17:01 +0100

    Configure Django project and add project documentation

---
+++ README.md
@@ -0,0 +1,25 @@
+# SmartAsset SRM
+
+## Technologies
+
+- Django REST Framework
+- PostgreSQL
+- React
+- JWT
+- Docker (later)
+
+## Installation
+
+```bash
+git clone <repo>
+
+cd SmartAsset-SRM
+
+python -m venv venv
+
+venv\Scripts\activate
+
+pip install -r requirements.txt
+
+python manage.py runserver
+```
--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
+    'users',
+    'equipments',
+    'reports',
+    'interventions',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -115,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+CORS_ALLOW_ALL_ORIGINS = True

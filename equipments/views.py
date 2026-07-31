5674906b1c68697f31c0f3d8f87236ab269f028c
Khaoula Laanait <khaoula.laanait@gmail.com>
2026-07-31 22:57:53 +0100

    Create equipment API with serializer and viewset

--- equipments/views.py
+++ equipments/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Equipment
+from .serializers import EquipmentSerializer
+
+
+class EquipmentViewSet(viewsets.ModelViewSet):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer

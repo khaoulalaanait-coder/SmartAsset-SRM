a688ff7dd99002b8cb00cba7a8d4b55149d8261c
Khaoula Laanait <khaoula.laanait@gmail.com>
2026-08-03 09:59:38 +0100

    Add Equipments API CRUD endpoints

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('equipments.urls')),
+    path('api/', include('reporting.urls')),
 ]
 
 if settings.DEBUG:

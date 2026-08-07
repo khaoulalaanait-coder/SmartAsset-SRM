461301e86c2325c5a9a4348d6b1037d77ff6aa52
boutamgarthafsa <boutamgarthafsa@gmail.com>
2026-08-07 10:22:19 +0100

    Configure report module views and main urls

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Report, Photo, ReportHistory
+from .serializers import ReportSerializer, PhotoSerializer, ReportHistorySerializer
 
-# Create your views here.
+class ReportViewSet(viewsets.ModelViewSet):
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+    permission_classes = [permissions.AllowAny]
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ReportHistoryViewSet(viewsets.ModelViewSet):
+    queryset = ReportHistory.objects.all()
+    serializer_class = ReportHistorySerializer
+    permission_classes = [permissions.AllowAny]
